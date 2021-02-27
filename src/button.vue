15fc9ef843c145fce14cbce315a85faa8dbb6809
Jerome <NeilCrofts@foxmail.com>
2021-02-27 19:38:13 +0800

    添加icon 和 iconPositon属性

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,38 @@
 <template>
-   <button class='g-button'>按钮</button>
+   <button class='g-button' :class="{[`icon-${iconPossition}`]:true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href='`#i-${icon}`'></use>
+        </svg> 
+        <div class="content">
+            <slot></slot>
+        </div>
+   </button>
 </template>
 <script>
 export default {
-    template: `<button class='g-button'>按钮</button>`
+    props:['icon','iconPossition']
 }
 </script>
 <style lang="scss">
     .g-button {
+        display: inline-flex;justify-content: center;align-items: center;  //用flex布局来设置图标在按钮的左右位置
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover {
-            border-color: var(--border-color-hover);
+        vertical-align:middle;
+        &:hover { border-color: var(--border-color-hover);}
+        &:active {background-color: var(--button-active-bg);}
+        &:focus {outline: none;}
+        //>为子类选择器 order1在前2在后
+        >.icon{ order: 1;margin-right: .3em;margin-left: 0;}
+        >.content{order:2;}
+        &.icon-right{
+            >.icon{order:2;margin-left: .3em;margin-right: 0;}
+            >.content{order: 1}
         }
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-        &:focus {
-            outline: none;
-        }
+        
     }
 </style>
