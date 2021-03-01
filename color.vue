f3b00b3e3853f96a48aec6eee09d1faf92c394c7
Jerome <NeilCrofts@foxmail.com>
2021-03-02 00:26:39 +0800

    添加 input 组件

---
+++ color.vue
@@ -0,0 +1,90 @@
+<style lang="css">
+    /* Color Theme Swatches in Hex */
+.Green-leaf绿叶-1-hex { color: #028C17; }
+.Green-leaf绿叶-2-hex { color: #017309; }
+.Green-leaf绿叶-3-hex { color: #6F8C42; }
+.Green-leaf绿叶-4-hex { color: #E6E4E3; }
+.Green-leaf绿叶-5-hex { color: #DB5D49; }
+
+/* Color Theme Swatches in RGBA */
+.Green-leaf绿叶-1-rgba { color: rgba(1, 140, 22, 1); }
+.Green-leaf绿叶-2-rgba { color: rgba(1, 114, 8, 1); }
+.Green-leaf绿叶-3-rgba { color: rgba(110, 140, 66, 1); }
+.Green-leaf绿叶-4-rgba { color: rgba(230, 228, 226, 1); }
+.Green-leaf绿叶-5-rgba { color: rgba(219, 93, 73, 1); }
+
+/* Color Theme Swatches in HSLA */
+.Green-leaf绿叶-1-hsla { color: hsla(129, 97, 27, 1); }
+.Green-leaf绿叶-2-hsla { color: hsla(124, 98, 22, 1); }
+.Green-leaf绿叶-3-hsla { color: hsla(83, 35, 40, 1); }
+.Green-leaf绿叶-4-hsla { color: hsla(20, 5, 89, 1); }
+.Green-leaf绿叶-5-hsla { color: hsla(8, 66, 57, 1); }
+
+
+</style>
+<style lang="scss">
+    /* Color Theme Swatches in Hex */
+$Green-leaf绿叶-1-hex: #028C17;
+$Green-leaf绿叶-2-hex: #017309;
+$Green-leaf绿叶-3-hex: #6F8C42;
+$Green-leaf绿叶-4-hex: #E6E4E3;
+$Green-leaf绿叶-5-hex: #DB5D49;
+
+/* Color Theme Swatches in RGBA */
+$Green-leaf绿叶-1-rgba: rgba(1,140,22, 1);
+$Green-leaf绿叶-2-rgba: rgba(1,114,8, 1);
+$Green-leaf绿叶-3-rgba: rgba(110,140,66, 1);
+$Green-leaf绿叶-4-rgba: rgba(230,228,226, 1);
+$Green-leaf绿叶-5-rgba: rgba(219,93,73, 1);
+
+/* Color Theme Swatches in HSLA */
+$Green-leaf绿叶-1-hsla: hsla(129, 97, 27, 1);
+$Green-leaf绿叶-2-hsla: hsla(124, 98, 22, 1);
+$Green-leaf绿叶-3-hsla: hsla(83, 35, 40, 1);
+$Green-leaf绿叶-4-hsla: hsla(20, 5, 89, 1);
+$Green-leaf绿叶-5-hsla: hsla(8, 66, 57, 1);
+</style>
+<style lang="css">
+    /* Color Theme Swatches in Hex */
+.Firenze-1-hex { color: #468966; }
+.Firenze-2-hex { color: #FFF0A5; }
+.Firenze-3-hex { color: #FFB03B; }
+.Firenze-4-hex { color: #B64926; }
+.Firenze-5-hex { color: #8E2800; }
+
+/* Color Theme Swatches in RGBA */
+.Firenze-1-rgba { color: rgba(70, 137, 102, 1); }
+.Firenze-2-rgba { color: rgba(255, 239, 165, 1); }
+.Firenze-3-rgba { color: rgba(255, 175, 59, 1); }
+.Firenze-4-rgba { color: rgba(182, 73, 38, 1); }
+.Firenze-5-rgba { color: rgba(142, 40, 0, 1); }
+
+/* Color Theme Swatches in HSLA */
+.Firenze-1-hsla { color: hsla(148, 32, 40, 1); }
+.Firenze-2-hsla { color: hsla(49, 100, 82, 1); }
+.Firenze-3-hsla { color: hsla(35, 100, 61, 1); }
+.Firenze-4-hsla { color: hsla(14, 65, 43, 1); }
+.Firenze-5-hsla { color: hsla(16, 100, 27, 1); }
+</style>
+<style lang="sass">
+    /* Color Theme Swatches in Hex */
+$Firenze-1-hex: #468966;
+$Firenze-2-hex: #FFF0A5;
+$Firenze-3-hex: #FFB03B;
+$Firenze-4-hex: #B64926;
+$Firenze-5-hex: #8E2800;
+
+/* Color Theme Swatches in RGBA */
+$Firenze-1-rgba: rgba(70,137,102, 1);
+$Firenze-2-rgba: rgba(255,239,165, 1);
+$Firenze-3-rgba: rgba(255,175,59, 1);
+$Firenze-4-rgba: rgba(182,73,38, 1);
+$Firenze-5-rgba: rgba(142,40,0, 1);
+
+/* Color Theme Swatches in HSLA */
+$Firenze-1-hsla: hsla(148, 32, 40, 1);
+$Firenze-2-hsla: hsla(49, 100, 82, 1);
+$Firenze-3-hsla: hsla(35, 100, 61, 1);
+$Firenze-4-hsla: hsla(14, 65, 43, 1);
+$Firenze-5-hsla: hsla(16, 100, 27, 1);
+</style>
--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import Button from './button'
 import icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 Vue.component('g-button', Button)
 Vue.component('g-icon', icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 var vm = new Vue({
     el: '#app',
     data: {
@@ -19,81 +21,4 @@ var vm = new Vue({
 import chai from 'chai';
 import spies from 'chai-spies'
 chai.use(spies)
-const expect = chai.expect;
-//单元测试
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting'
-        }
-    })
-    vm.$mount('')
-    let useElement = vm.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-setting')
-    vm.$el.remove()
-    vm.$destroy()
-} {
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-            isLoading: true
-        }
-    })
-    vm.$mount('')
-    let useElement = vm.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-    vm.$el.remove()
-    vm.$destroy()
-} {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-        }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-    vm.$el.remove()
-    vm.$destroy()
-} {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-            iconPosition: 'right'
-        }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-
-{
-
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-        }
-    })
-    vm.$mount()
-    const spy = chai.spy(() => {})
-    vm.$on('click', spy)
-    let button = vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
-}
+    // const expect = chai.expect;
