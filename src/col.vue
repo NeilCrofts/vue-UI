fb18495e178b880a621894d8b3a1517f5f35abdd
Jerome <NeilCrofts@foxmail.com>
2021-03-03 10:55:17 +0800

    初步实现响应式

--- src/col.vue
+++ src/col.vue
@@ -5,6 +5,14 @@
     </div>
 </template>
 <script>
+ let validator = (value)=>{
+      let keys = Object.keys(value)
+      let valid = true
+      if(!['span','offset'].includes(keys)){
+        valid = false 
+      }
+      return valid
+    }
 export default {
     props:{
         span:{
@@ -12,7 +20,12 @@ export default {
         },
         offset:{
             type:[String,Number]
-        }
+        },
+      phone: {type: Object, validator,},
+      ipad: {type: Object, validator,},
+      narrowPc: {type: Object, validator,},
+      pc: {type: Object, validator,},
+      widePc: {type: Object, validator,}
     },
     data(){
         return{
@@ -27,8 +40,16 @@ export default {
                 }
             },
             colClass(){
-                let {span,offset} = this
-                return [span && `col-${span}`,offset && `offset-${offset}`]
+                let {span,offset,phone,ipad,narrowPc,pc,widePc} = this
+                return [
+                  span && `col-${span}`,
+                  offset && `offset-${offset}`,
+                  ...(phone && `col-phone-${phone.span}`),
+                  ...(ipad && [`col-ipad-${ipad.span}`]),
+                  ...(narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
+                  ...(pc && [`col-pc-${pc.span}`]),
+                  ...(widePc && [`col-wide-pc-${widePc.span}`]),
+                ]
             }
     },
    
@@ -46,12 +67,83 @@ export default {
         width: ($n / 24) * 100%;
       }
     }
-
     $class-prefix: offset-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
       }
     }
+    //响应式 媒体查询
+    @media (max-width: 576px) {
+      $class-prefix: col-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 577px) and (max-width: 768px) {
+      $class-prefix: col-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 769px) and (max-width: 992px) {
+      $class-prefix: col-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 993px) and (max-width: 1200px) {
+      $class-prefix: col-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 1201px) {
+      $class-prefix: col-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+
   }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -42,6 +42,7 @@ export default {
     
     .row{
         display: flex;
+        flex-wrap: wrap;
         &.align-left {
             justify-content: flex-start;
         }
