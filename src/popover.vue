54a96712675f58fc0a5bb3e2a1b44426af276e55
Jerome <NeilCrofts@foxmail.com>
2021-03-08 14:29:58 +0800

    重构代码，功能方面bug基本解决

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="popover" @click="onClick">
-        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
         <!-- 添加span为获取slot或确定slot位置 -->
@@ -16,28 +16,37 @@ export default {
         return {visible:false}
     },
     methods:{
+        close(){
+            this.visible=false
+            document.removeEventListener('click',this.onClickDocument)
+        },
+        contentPosition(){
+            //移动一个元素的位置，不会影响其功能，绑定的事件也能触发
+            document.body.append(this.$refs.contentWrapper);
+            //确定button位置--》进而设置popover内容位置
+            let {left,top,width} = this.$refs.triggerWrapper.getBoundingClientRect();
+            this.$refs.contentWrapper.style.left = left+window.scrollX+'px';
+            this.$refs.contentWrapper.style.top = top-width+window.scrollY+'px';
+        },
+        onClickDocument(e){//点击按钮后再点击一次，还会触发该事件，因为第一次按钮生成的document监听事件还没有移除，再点击按钮冒泡触发了该事件
+            //不用冒泡，如果是popover弹窗内容，则不关闭
+            if(this.$refs.contentWrapper && this.$refs.contentWrapper.contains(e.target)){return}
+                this.close()
+            },
+        open(){
+            this.visible = true
+             setTimeout(()=>{
+                this.contentPosition();
+                //当onClick方法一执行，该click事件就新建并调用了，故需要用异步
+                document.addEventListener('click',this.onClickDocument)
+            })
+        },
         onClick(event){
-            // console.log(event.target);
             if(this.$refs.triggerWrapper.contains(event.target)){
-                this.visible=!this.visible
-                console.log(this.visible,'切换visible');
             if(this.visible===true){
-              setTimeout(()=>{
-                   //移动一个元素的位置，不会影响其功能，绑定的事件也能触发
-                    document.body.append(this.$refs.contentWrapper);
-                    //确定button位置--》进而设置popover内容位置
-                    let {left,top,width} = this.$refs.triggerWrapper.getBoundingClientRect();
-                    this.$refs.contentWrapper.style.left = left+window.scrollX+'px';
-                    this.$refs.contentWrapper.style.top = top-width+window.scrollY+'px';
-                    let eventHandle = ()=>{//点击按钮后再点击一次，还会触发该事件，因为第一次按钮生成的document监听事件还没有移除，再点击按钮冒泡触发了该事件
-                        this.visible = false
-                        console.log(this.visible,'点击body关闭popover')
-                        document.removeEventListener('click',eventHandle)
-                }
-                   //当onClick方法一执行，该click事件就新建并调用了，故需要用异步
-                    document.addEventListener('click',eventHandle)
-                    
-                })
+                this.close();
+            }else{
+                this.open();
             }
         }
     },
