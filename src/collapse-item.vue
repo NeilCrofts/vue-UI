f5a32d30bbd18d16234babb7c9bfca8f42a07810
Jerome <NeilCrofts@foxmail.com>
2021-03-09 20:04:26 +0800

    可以设置默认 selected

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -14,6 +14,10 @@ export default {
             type:String,
             required:true
         },
+        name:{
+            type:String,
+            require:true
+        }
     },
     data(){
         return{
@@ -21,23 +25,27 @@ export default {
         }
     },
     mounted(){
-        this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
-            if(vm!==this){
+        this.eventBus && this.eventBus.$on('update:selected',(name)=>{
+            if(name!==this.name){
                 this.close()
+            }else{
+                this.show()
             }
         })
     },
     methods:{
         toggleState(){
             if(this.open){
-                this.open = !this.open
+                this.open = false
             }else{
-                this.open = true
-                this.eventBus && this.eventBus.$emit('update:selected',this)
+                this.eventBus && this.eventBus.$emit('update:selected',this.name)
             }
         },
         close(){
             this.open = false
+        },
+        show(){
+            this.open = true
         }
     },
     inject:['eventBus']
